2c359cb93a9aa9a3a02c67c84477d9708317a207
Nelsordo <nelson.alexander.ureta.barra@gmail.com>
2026-06-26 19:15:27 -0400

    depositos añadidos a transferencias (movimientos de la cuenta)

--- M2/sources/deposit.js
+++ M2/sources/deposit.js
@@ -4,9 +4,28 @@ function depositar(monto) {
   localStorage.setItem("saldo", saldo);
 }
 
+function guardarDeposito(monto){
+  let transferencias = JSON.parse(localStorage.getItem("transferencias")) || [];
+  let nuevoDeposito = {
+    nombre:"Depósito en efectivo",
+    CBU:"N/A",
+    alias:"N/A",
+    banco:"N/A"
+  };
+  nuevoDeposito.monto= monto; 
+  transferencias.push(nuevoDeposito);
+  localStorage.setItem("transferencias", JSON.stringify(transferencias));
+}
+
 document.getElementById("deposit").addEventListener("click",(e)=>{
     e.preventDefault();
     let monto = parseInt(document.getElementById("depositAmount").value);
-    depositar(monto);
-    window.location.href = "./menu.html";
+    if(monto>0){
+      depositar(monto);
+      guardarDeposito(monto);
+      window.location.href = "./menu.html";
+    }else{
+      alert("Deposite una cantidad válida");
+    }
+    
 });
