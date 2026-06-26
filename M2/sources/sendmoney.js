1244b65dbb7f2e895ac7975735507da533a73d0c
Nelsordo <nelson.alexander.ureta.barra@gmail.com>
2026-06-26 17:50:12 -0400

    Funcionalidad de transferencias listas, memoria de transferencias y contactos creadas

--- M2/sources/sendmoney.js
+++ M2/sources/sendmoney.js
@@ -30,7 +30,7 @@ function mostrarContactos() {
     listaDoc.innerHTML += `
       <li class="list-group-item">
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="contacto" id="c${index}" value="${contacto.alias}">
+          <input class="form-check-input" type="radio" name="contacto" id="c${index}" value="${index}">
           <label class="form-check-label" for="c${index}">
             <span class="contact-name">${contacto.nombre}</span><br>
             <small class="text-muted">CBU: ${contacto.cbu}, Alias: ${contacto.alias}, Banco: ${contacto.banco}</small>
@@ -40,7 +40,56 @@ function mostrarContactos() {
     `;
   });
 }
+
+function seleccionContacto() {
+  let seleccionadoIndex = document.getElementById("contactSelect").querySelector("input:checked").value;
+  let transferir = document.getElementById("transferir");
+
+  let listaContactos = JSON.parse(localStorage.getItem("listaContactos")) || [];
+  let contacto = listaContactos[seleccionadoIndex];
+
+  transferir.innerHTML = `
+          <span class="contact-name">${contacto.nombre}</span><br>
+          <small class="text-muted">CBU: ${contacto.cbu}, Alias: ${contacto.alias}, Banco: ${contacto.banco}</small>
+  `;
+  document.getElementById("transferenciaCard").style.display = "block";
+
+  return contacto;
+}
+
+function transferencia(contacto,monto){
+  let saldo = parseInt(localStorage.getItem("saldo")) || 0;
+
+    console.log("Saldo:", saldo);
+    console.log("Monto:", monto);
+
+  if (saldo>monto) {
+    saldo -= monto;
+    localStorage.setItem("saldo", saldo);
+    
+    guardarTransferencia(contacto,monto);
+
+    alert("Transferencia exitosa\nRedirigiendo al menú principal");
+    window.location.href="./menu.html";
+    
+  } else {
+    alert("Saldo insuficiente\nSaldo disponible:"+saldo);
+  }
+
+
+}
+
+function guardarTransferencia(contacto,monto){
+  let transferencias = JSON.parse(localStorage.getItem("transferencias")) || [];
+  let nuevaTransferencia = contacto;
+  nuevaTransferencia.monto= monto; 
+  transferencias.push(nuevaTransferencia);
+  localStorage.setItem("transferencias", JSON.stringify(transferencias));
+}
+
 //-------------------------------CODIGO----------------------------------------------
+
+var contactoTransfer;
 
 //Hace aparecer el formulario
 document.getElementById("agregarContacto").addEventListener("click",(e)=>{
@@ -61,6 +110,30 @@ document.getElementById("contactForm").addEventListener("submit",(e)=>{
     document.getElementById("agregarContacto").style.display = "block"; // muestra el botón de nuevo
     mostrarContactos();
 });
+
+//selecciona el contacto
+
+document.getElementById("contactSelect").addEventListener("submit",(e)=>{
+  e.preventDefault();
+  document.getElementById("principalDeposit").style.display = "none";
+  contactoTransfer = seleccionContacto();
+});
+
+
+//Realiza la transferencia y la guarda para ultimos movimientos
+document.getElementById("formDepositar").addEventListener("submit",(e)=>{
+  e.preventDefault();
+  let monto = parseInt(document.getElementById("monto").value);
+  transferencia(contactoTransfer,monto);
+});
+
+//volver a seleccionar contactos
+document.getElementById("volver").addEventListener("click",()=>{
+
+  document.getElementById("principalDeposit").style.display = "block";
+  document.getElementById("transferenciaCard").style.display = "none";
+});
+
 
 //hace aparecer la lista de contactos seleccionable
 document.addEventListener("DOMContentLoaded", () => {
